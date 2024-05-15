829c1ee4bb689b59024e35678311866f3560f664
jhordy12345 <136488834+jhordy12345@users.noreply.github.com>
2024-05-15 15:32:51 -0500

    nosotros

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8" />
+        <title> Xeloro - Admin & Dashboard Template</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="MyraStudio" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+        <!-- App css -->
+        <link href="plantilla/admin/vertical/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="plantilla/admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="plantilla/admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body>
+
+        <!-- Begin page -->
+        <div id="layout-wrapper">
+            <div class="header-border"></div>
+            
+
+            <?php
+                include ("include/menu.php");
+            ?>
+            <!-- ============================================================== -->
+            <!-- Start right Content here -->
+            <!-- ============================================================== -->
+            <div class="main-content">
+
+                <div class="page-content">
+                    <div class="container-fluid">
+                        <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-body">
+                    
+                                    <h4 class="card-title">With Indicators</h4>
+                                    <p class="card-subtitle mb-4">You can also add the indicators to the carousel, alongside the controls, too.</p>
+                                    
+                                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                        <ol class="carousel-indicators">
+                                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                        </ol>
+                                        <div class="carousel-inner" role="listbox">
+                                            <div class="carousel-item active">
+                                                <img class="d-block img-fluid" src="https://static.vecteezy.com/system/resources/previews/026/481/726/large_2x/a-spectacular-gaming-adventure-with-this-stunning-4k-wallpaper-free-photo.jpg" alt="First slide">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block img-fluid" src="https://static.vecteezy.com/system/resources/previews/026/481/506/non_2x/a-spectacular-gaming-adventure-with-this-stunning-4k-wallpaper-free-photo.jpg" alt="Second slide">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block img-fluid" src="https://images.hdqwalls.com/wallpapers/hyper-scape-season-2-4k-y7.jpg" alt="Third slide">
+                                            </div>
+                                        </div>
+                                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
+                                    </div>
+                                </div> <!-- end card-body-->
+                            </div> <!-- end card-->
+                        </div> <!-- end col -->
+                        </div>
+
+                       
+
+                    </div> <!-- container-fluid -->
+                </div>
+                <!-- End Page-content -->
+
+                <footer class="footer">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                2020 © Xeloro.
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="text-sm-right d-none d-sm-block">
+                                    Diseño y desarrollo por Myra
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+
+            </div>
+            <!-- end main content-->
+
+        </div>
+        <!-- END layout-wrapper -->
+
+        <!-- Overlay-->
+        <div class="menu-overlay"></div>
+
+
+        <!-- jQuery  -->
+        <script src="plantilla/admin/vertical/assets/js/jquery.min.js"></script>
+        <script src="plantilla/admin/vertical/assets/js/bootstrap.bundle.min.js"></script>
+        <script src="plantilla/admin/vertical/assets/js/metismenu.min.js"></script>
+        <script src="plantilla/admin/vertical/assets/js/waves.js"></script>
+        <script src="plantilla/admin/vertical/assets/js/simplebar.min.js"></script>
+
+        <!-- App js -->
+        <script src="plantilla/admin/vertical/assets/js/theme.js"></script>
+
+    </body>
+
+</html>
---
+++ nosotros.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8" />
+        <title> Xeloro - Admin & Dashboard Template</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="MyraStudio" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+        <!-- App css -->
+        <link href="plantilla/admin/vertical/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="plantilla/admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="plantilla/admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body>
+
+        <!-- Begin page -->
+        <div id="layout-wrapper">
+            <div class="header-border"></div>
+            
+
+            <?php
+                include ("include/menu.php");
+            ?>
+            <!-- ============================================================== -->
+            <!-- Start right Content here -->
+            <!-- ============================================================== -->
+<div class="main-content">
+
+<div class="page-content">
+    <div class="container-fluid">
+
+        <!-- start page title -->
+        <div class="row">
+        
+            <div class="col-12">
+                <div class="page-title-box d-flex align-items-center justify-content-between">
+                    <h4 class="mb-0 font-size-18">Preguntas frecuentes</h4>
+
+                    <div class="page-title-right">
+                        <ol class="breadcrumb m-0">
+                            <li class="breadcrumb-item"><a href="javascript: void(0);">paginas</a></li>
+                            <li class="breadcrumb-item active">Preguntas frecuentes</li>
+                        </ol>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>      
+        <!-- end page title -->
+
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="text-center mt-4">
+                    <h4>Nosotros</h4>
+                    <p class="text-muted mt-3 mb-4"> En Tecno Pro, nos dedicamos a ofrecer una amplia gama de equipos electrónicos y tecnológicos de <br/> 
+                    alta calidad. Nuestro objetivo es proporcionar a nuestros clientes productos innovadores y fiables,<br/>junto con un servicio al cliente excepcional </p>
+
+                </div>
+            </div><!-- end col -->
+        </div><!-- end row -->
+
+
+        <div class="row mt-5">
+            <div class="col-lg-8">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="mb-3 font-weight-bold text-uppercase">¿Quiénes Somos?</h5>
+                        <p>Somos una empresa comprometida con la excelencia en la comercialización de equipos electrónicos.
+                             Con una trayectoria que nos respalda, hemos establecido fuertes relaciones con los principales fabricantes
+                              del sector para garantizar la mejor calidad en nuestros productos.</p>
+                        <div id="accordion">
+                            <div class="card mb-1">
+                                <div class="card-header bg-white border-bottom-0 p-3" id="headingOne">
+                                    <h5 class="m-0 font-size-16">
+                                        <a href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true"
+                                            aria-controls="collapseOne" class="text-dark">
+                                            Nuestra Misión
+                                        </a>
+                                    </h5>
+                                </div>
+
+                                <div id="collapseOne" class="collapse show"
+                                        aria-labelledby="headingOne" data-parent="#accordion">
+                                    <div class="card-body text-muted pt-1">
+                                    Nuestra misión es ser el proveedor preferido de equipos electrónicos, 
+                                    ofreciendo una experiencia de compra inigualable, tanto en línea como
+                                     en nuestras tiendas físicas. Nos esforzamos por mantenernos a la 
+                                     vanguardia de las tendencias tecnológicas para satisfacer las necesidades
+                                      cambiantes de nuestros clientes.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card mb-1">
+                                <div class="card-header bg-white border-bottom-0 p-3" id="headingTwo">
+                                    <h5 class="m-0 font-size-16">
+                                        <a href="#" class="text-dark collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        Lo Que Ofrecemos
+                                        </a>
+                                    </h5>
+                                </div>
+                                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                                    <div class="card-body text-muted pt-1">
+                                    Computadoras y Laptops: Desde modelos para uso doméstico hasta equipos de alta gama para profesionales.
+                                    Dispositivos Móviles: Una selección de smartphones y tabletas de las marcas más reconocidas.
+                                    Accesorios y Periféricos: Todo lo necesario para complementar y mejorar el uso de tus dispositivos electrónicos.
+                                    Electrodomésticos Inteligentes: Soluciones tecnológicas para el hogar que facilitan la vida cotidiana.
+                                    Componentes Electrónicos: Para aquellos que buscan piezas específicas para proyectos DIY o reparaciones.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card mb-1">
+                                <div class="card-header bg-white border-bottom-0 p-3" id="headingThree">
+                                    <h5 class="m-0 font-size-16">
+                                        <a href="#" class="text-dark collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                        Nuestro Compromiso
+                                        </a>
+                                    </h5>
+                                </div>
+                                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                                    <div class="card-body text-muted pt-1">
+                                    En Tecno Pro, nos comprometemos a:
+
+                                Calidad y Fiabilidad: Solo trabajamos con proveedores de confianza para garantizar
+                                 la calidad de nuestros productos.
+                                Innovación Constante: Nos mantenemos actualizados con las últimas innovaciones
+                                 tecnológicas para ofrecer siempre lo mejor.
+                                Servicio al Cliente: Nuestro equipo de soporte está siempre disponible para ayudar
+                                 y asesorar a nuestros clientes en todo lo que necesiten.
+                                Precios Competitivos: Ofrecemos precios justos y competitivos para garantizar que
+                                 nuestros clientes obtengan el mejor valor por su dinero.
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
+                <
+            </div> <!-- end col -->
+
+        <!-- Overlay-->
+        <div class="menu-overlay"></div>
+
+
+        <!-- jQuery  -->
+        <script src="plantilla/admin/vertical/assets/js/jquery.min.js"></script>
+        <script src="plantilla/admin/vertical/assets/js/bootstrap.bundle.min.js"></script>
+        <script src="plantilla/admin/vertical/assets/js/metismenu.min.js"></script>
+        <script src="plantilla/admin/vertical/assets/js/waves.js"></script>
+        <script src="plantilla/admin/vertical/assets/js/simplebar.min.js"></script>
+
+        <!-- App js -->
+        <script src="plantilla/admin/vertical/assets/js/theme.js"></script>
+
+    </body>
+
+</html>
